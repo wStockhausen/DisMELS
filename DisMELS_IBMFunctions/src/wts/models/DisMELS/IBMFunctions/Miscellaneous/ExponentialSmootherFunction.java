c3b37959f8f1aad337d555d8f2094b9e2f6cf15d
wStockhausen <william.stockhausen@noaa.gov>
2019-06-26 07:38:09 -0700

    Added ExponentialSmootherFunction to DisMELS_IBMFunctions module.

--- DisMELS_IBMFunctions/src/wts/models/DisMELS/IBMFunctions/Miscellaneous/ExponentialSmootherFunction.java
+++ DisMELS_IBMFunctions/src/wts/models/DisMELS/IBMFunctions/Miscellaneous/ExponentialSmootherFunction.java
@@ -1,5 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package wts.models.DisMELS.IBMFunctions.Miscellaneous;
@@ -11,7 +12,7 @@ import wts.models.DisMELS.framework.IBMFunctions.IBMFunctionInterface;
 import wts.models.DisMELS.framework.IBMFunctions.IBMParameter;
 
 /**
- * This class provides an implementation of a constant function.
+ * This class provides an implementation of an exponential smoothing function.
  * Type: 
  *      generic function
  * Parameters (by key):
@@ -19,34 +20,35 @@ import wts.models.DisMELS.framework.IBMFunctions.IBMParameter;
  * Variables:
  *      vars - null
  * Value:
- *      the constant - Double
+ *      the smoothed value - Double
  * 
- * @author William.Stockhausen
+ * @author William Stockhausen
  */
 @ServiceProviders(value={
     @ServiceProvider(service=IBMFunctionInterface.class)}
 )
-public class ConstantFunction extends AbstractIBMFunction {
+public class ExponentialSmootherFunction extends AbstractIBMFunction {
+    
     
     /** function classification */
     public static final String DEFAULT_type = "generic";
     /** user-friendly function name */
-    public static final String DEFAULT_name = "constant function";
+    public static final String DEFAULT_name = "exponential smoothing function";
     /** function description */
-    public static final String DEFAULT_descr = "returns a constant value";
+    public static final String DEFAULT_descr = "returns an exponentially-smoothed value";
     /** full description */
     public static final String DEFAULT_fullDescr = 
             "\n\t**************************************************************************"+
-            "\n\t* This class provides an implementation of a constant function."+
+            "\n\t* This class provides an implementation of an exponential smoothing function."+
             "\n\t* Type: "+
             "\n\t*      generic function"+
             "\n\t* Parameters (by key):"+
-            "\n\t*      constant - Double - the constant value"+
+            "\n\t*      tau - Double: the value of the time constant, in days"+
             "\n\t* Variables:"+
-            "\n\t*      vars - any Object or null"+
+            "\n\t*      vars - Double[]: {value to be smoothed, time step (in days)}"+
             "\n\t* Value:"+
-            "\n\t*      the constant value"+
-            "\n\t* author: William.Stockhausen"+
+            "\n\t*      the smoothed value"+
+            "\n\t* author: William Stockhausen"+
             "\n\t**************************************************************************";
 
     /** number of settable parameters */
@@ -54,22 +56,24 @@ public class ConstantFunction extends AbstractIBMFunction {
     /** number of sub-functions */
     public static final int numSubFuncs = 0;
 
-    /** key to set the constant parameter */
-    public static final String PARAM_constant = "the constant value";
+    /** key to set the time constant parameter */
+    public static final String PARAM_timeConstant = "tau (time constant in days)";
     
-    /** value of the constant parameter */
-    private double constant = 0.0;
-   
+    /** value of the time constant */
+    protected double tau = 0.0;
+    /** value of the smoother */
+    protected double value = Double.NaN;
+    
     /** constructor for class */
-    public ConstantFunction(){
+    public ExponentialSmootherFunction(){
         super(numParams,numSubFuncs,DEFAULT_type,DEFAULT_name,DEFAULT_descr,DEFAULT_fullDescr);
         String key; 
-        key = PARAM_constant;  addParameter(key,Double.class, key);
+        key = PARAM_timeConstant;  addParameter(key,Double.class, key);
     }
     
     @Override
-    public ConstantFunction clone(){
-        ConstantFunction clone = new ConstantFunction();
+    public ExponentialSmootherFunction clone(){
+        ExponentialSmootherFunction clone = new ExponentialSmootherFunction();
         clone.setFunctionType(getFunctionType());
         clone.setFunctionName(getFunctionName());
         clone.setDescription(getDescription());
@@ -85,18 +89,18 @@ public class ConstantFunction extends AbstractIBMFunction {
     }
     
     /**
-     * Sets the parameter value corresponding to the key associated with param.
+     * Sets the parameter value corresponding to the key given by param.
      * 
      * @param param - the parameter key (name)
      * @param value - its value
-     * @return 
+     * @return - true/false whether value was set
      */
     @Override
     public boolean setParameterValue(String param,Object value){
         if (super.setParameterValue(param, value)){
             switch (param) {
-                case PARAM_constant:
-                    constant = ((Double) value).doubleValue();
+                case PARAM_timeConstant:
+                    tau = ((Double) value);
                     break;
             }
             return true;
@@ -107,12 +111,18 @@ public class ConstantFunction extends AbstractIBMFunction {
     /**
      * Calculates the value of the function.
      * 
-     * @param vars - any Object or null
-     * 
-     * @return     - Double - the constant value
+     * @param vars - Double[]: {value to be smoothed, time step (in days)}
+     * @return     - Double: the smoothed value
      */
     @Override
     public Double calculate(Object vars) {
-        return new Double(constant);
+        Double[] vals = (Double[]) vars;
+        if (Double.isNaN(value)){
+            value = vals[0];
+        } else {
+            double lam = -Math.expm1(-vals[1]/tau);//=(1-exp(-dt/tau))
+            value = lam*vals[0] + (1.0-lam)*value;
+        }
+        return value;
     }
 }
